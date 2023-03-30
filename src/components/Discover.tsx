89fcd8a44a7746279aa42608bb9756d6d1f2739c
Christian Fenn <cfenn2020@fau.edu>
2023-03-30 12:55:28 -0400

    Completed Sidebar Template

---
+++ src/components/Discover.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { topics } from '@/utils/constants';
+
+type Props = {}
+
+const Discover = (props: Props) => {
+  return (
+    <div>Discover</div>
+  )
+}
+
+export default Discover
