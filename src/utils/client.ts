092eea9682197bd7c92fc74fa9941afbfc864106
Christian Fenn <cfenn2020@fau.edu>
2023-03-30 12:23:06 -0400

    Completed Initial Project Setup

---
+++ src/utils/client.ts
@@ -0,0 +1,9 @@
+import { createClient } from '@sanity/client';
+
+export const client = createClient({
+  projectId: 'tp4ln3j8',
+  dataset: 'production',
+  apiVersion: '2022-03-10',
+  useCdn: false,
+  token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
+});
